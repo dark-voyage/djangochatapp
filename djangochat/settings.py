a4b6140ef12fc88f2fbee77177350a4027f1502d
genemators <yahyobek0099@gmail.com>
2021-10-31 23:43:25 +0500

    collectstatic

--- djangochat/settings.py
+++ djangochat/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os.path
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -27,7 +27,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['https://chatforus.herokuapp.com/', 'localhost', '127.0.0.1']
 
-
+STATIC_ROOT=os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL='/static/'
 # Application definition
 
 INSTALLED_APPS = [
